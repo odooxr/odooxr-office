ebc2a5e3f09aeb2a16f787874cb2f1ba53f5f30a
Cristian S. Rocha <cristian.rocha@moldeo.coop>
2022-02-05 23:04:01 -0300

    First commit from odooxr-base split

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Metaverse Office",
+
+    'summary': """Work in the metaverse""",
+
+    'description': """
+        All your Odoo forms, reports, lists and more around you.
+    """,
+
+    'author': "Cristian S. Rocha",
+    'website': "https://github.com/odooxr",
+
+    'category': 'Productivity/Office',
+    'license': 'LGPL-3',
+    'version': '0.1',
+
+    'installable': True,
+    'application': True,
+
+    'depends': ['base', 'odooxr-base'],
+
+    'data': [],
+    'demo': [],
+}
